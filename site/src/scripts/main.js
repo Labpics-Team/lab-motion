8d3f4c5b51171d5a9580c2d24ad0694bd6faa6c2
Daniel from Labpics <63733699+lemone112@users.noreply.github.com>
2026-09-21 12:55:52 +0300

    tooling: подключить compiler playground

--- site/src/scripts/main.js
+++ site/src/scripts/main.js
@@ -1,5 +1,12 @@
+import { installCompilerPlayground } from './compiler-playground.js';
 import { installShowcase } from './showcase.js';
 
-const dispose = installShowcase();
+const disposeShowcase = installShowcase();
+const disposeCompilerPlayground = installCompilerPlayground();
+
+const dispose = () => {
+  disposeCompilerPlayground();
+  disposeShowcase();
+};
 
 if (import.meta.hot) import.meta.hot.dispose(dispose);
